155d03a12358db7e4ebf02eccfdcb9ecd5a3aba6
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-07-15 12:36:31 +0200

    solution of React 07 quest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sampleSimpson = {
+  character: "Nelson Muntz",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+  quote: "Ha ha!"
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: sampleSimpson
+    }
+
+    this.getQuotes = this.getQuotes.bind(this);
+  }
+
+  getQuotes() {
+    //Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      //Extract the DATA from the received response
+      .then(response => response.data)
+      //Use this data to update the state
+      .then(data => {
+        this.setState({
+          quotes: data[0],
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Simpson Quotes App</h1>
+        <p><strong>{this.state.quotes.character}</strong></p>
+        <p>{this.state.quotes.quote}</p>
+        <img src={this.state.quotes.image} />
+        <button type="button" onClick={this.getQuotes}>Get Quotes</button>
+      </div>
+    );
+  }
 }
 
 export default App;
